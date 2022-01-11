ea30b5c9085dca51b63a1913febcbc7bd81a142b
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2022-01-11 12:10:15 +0200

    Update archive

--- ckanext/iso19115/tests/conftest.py
+++ ckanext/iso19115/tests/conftest.py
@@ -1,5 +1,5 @@
 
-
+import pickle
 from pathlib import Path
 import pytest
 import xmlschema
@@ -8,11 +8,32 @@ import xmlschema
 def examples():
     return Path(__file__).parent / "examples"
 
+def _repo_schema() -> xmlschema.XMLSchema:
+    return xmlschema.XMLSchema(Path(__file__).parent.parent.parent.parent / "XML/standards.iso.org/iso/19115/-3/mdb/2.0/mdb.xsd")
+
+
+def _download_schema(schema: xmlschema.XMLSchema):
+    schema.export(str(Path(__file__).parent / "xsd"), True)
+
+
+def _downloaded_schema() -> xmlschema.XMLSchema:
+    return xmlschema.XMLSchema(Path(__file__).parent / "xsd/mdb.xsd")
+
+def _serialize_schema(schema: xmlschema.XMLSchema):
+    with (Path(__file__).parent / "mdb.pkl").open("wb") as dest:
+        pickle.dump(schema, dest)
+
+
+def _serialized_schema() -> xmlschema.XMLSchema:
+    with (Path(__file__).parent / "mdb.pkl").open("rb") as src:
+        return pickle.load(src)
+
+
+def _namespace_schema() -> xmlschema.XMLSchema:
+    return xmlschema.XMLSchema(Path(__file__).parent.parent.parent.parent / "namespaces/19115/-3/mdb/2.0/mdb.xsd")
+
+
 @pytest.fixture(scope="session")
 def mdb_schema():
-    # schema = xmlschema.XMLSchema(Path(__file__).parent.parent.parent.parent / "XML/standards.iso.org/iso/19115/-3/mdb/2.0/mdb.xsd")
-    # schema.export(Path(__file__).parent / "xsd", True)
-    # return schema
-    # return xmlschema.XMLSchema(Path(__file__).parent / "xsd/mdb.xsd")
-
-    return xmlschema.XMLSchema(Path(__file__).parent.parent.parent.parent / "namespaces/19115/-3/mdb/2.0/mdb.xsd")
+    schema =  _serialized_schema()
+    return schema
